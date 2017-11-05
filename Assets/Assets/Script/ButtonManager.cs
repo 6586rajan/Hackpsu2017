c956f105ee310fe2fa05ff868ec4f7fa5e08a40f
Rajan Srimat <6586rajan@gmail.com>
2017-11-05 13:54:24 -0500

    first commit

---
+++ Assets/Assets/Script/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public Canvas menu;
+
+    public void NewGameBtn(string newGame)
+    {
+        SceneManager.LoadSceneAsync(newGame);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
